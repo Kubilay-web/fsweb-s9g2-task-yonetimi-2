d5798530f2a5577a1a134b95003ef223dc31c3d9
Kubilay Özdemir <kubilayozdemir95@gmail.com>
2023-07-25 23:57:29 +0300

    Update

--- src/Task.js
+++ src/Task.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { formatDistanceToNow,differenceInDays} from 'date-fns';
+import { formatDistanceToNow, differenceInDays } from 'date-fns';
 
 
 const Task = ({ taskObj, onComplete }) => {
@@ -10,22 +10,31 @@ const Task = ({ taskObj, onComplete }) => {
 
   let backgroundColor;
   if (daysToDeadline <= 3) {
-    backgroundColor = '#ffd9d4'; 
+    backgroundColor = '#ffd9d4';
   } else {
-    backgroundColor = 'transparent'; 
+    backgroundColor = 'transparent';
   }
 
   return (
-    <div className="task" >
-      <h3>{taskObj.title}</h3>
-      <div className="deadline">son teslim:<span style={{backgroundColor}}>{formattedDistanceToNow}</span></div>
-      <p>{taskObj.description}</p>
-      <div>
+    <div className="p-6 bg-white rounded-md shadow-md mt-4">
+      <h3 className="text-xl text-yellow-600">{taskObj.title}</h3>
+      <div className="text-xs pt-1">
+        Son Teslim: <span className="px-2 py-1 rounded bg-pink-200" style={{ backgroundColor }}>{formattedDistanceToNow}</span>
+      </div>
+      <p className="pt-2 text-sm text-gray-700">{taskObj.description}</p>
+      <div className="flex mt-3">
         {taskObj.people.map((p) => (
-          <span className="pill" key={p}>{p}</span>
+          <span className="inline-block px-3 py-1 text-sm border border-gray-300 rounded-full mr-1 mb-2" key={p}>{p}</span>
         ))}
       </div>
-      {onComplete && <button onClick={() => onComplete(taskObj.id)}>Tamamlandı</button>}
+      {onComplete && (
+        <button
+          onClick={() => onComplete(taskObj.id)}
+          className="block mt-4 px-3 py-2 bg-yellow-300 shadow-sm rounded"
+        >
+          Tamamlandı
+        </button>
+      )}
     </div>
   );
 };
